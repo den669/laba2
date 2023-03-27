7f9f316083832fe3f53157abb8c1b84fb1981a0f
den669 <71308629+den669@users.noreply.github.com>
2023-03-28 01:07:22 +0300

    Add files via upload

---
+++ laba2/ClassLibrary1/Class1.cs
@@ -0,0 +1,79 @@
+﻿// Абстрактний клас Button
+abstract class Button
+{
+    public abstract void Paint();
+}
+
+// Клас ButtonFactory
+abstract class ButtonFactory
+{
+    public abstract Button CreateButton();
+}
+
+// Конкретні класи Button
+class WindowsButton : Button
+{
+    public override void Paint()
+    {
+        Console.WriteLine("Render a button in Windows style.");
+    }
+}
+
+class IOSButton : Button
+{
+    public override void Paint()
+    {
+        Console.WriteLine("Render a button in iOS style.");
+    }
+}
+
+class AndroidButton : Button
+{
+    public override void Paint()
+    {
+        Console.WriteLine("Render a button in Android style.");
+    }
+}
+
+// Конкретні фабрики для стилів кнопок
+class WindowsButtonFactory : ButtonFactory
+{
+    public override Button CreateButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+class IOSButtonFactory : ButtonFactory
+{
+    public override Button CreateButton()
+    {
+        return new IOSButton();
+    }
+}
+
+class AndroidButtonFactory : ButtonFactory
+{
+    public override Button CreateButton()
+    {
+        return new AndroidButton();
+    }
+}
+
+// Клієнтський код
+class Client
+{
+    private ButtonFactory buttonFactory;
+    private Button button;
+
+    public Client(ButtonFactory factory)
+    {
+        buttonFactory = factory;
+    }
+
+    public void Run()
+    {
+        button = buttonFactory.CreateButton();
+        button.Paint();
+    }
+}
---
+++ laba2/laba2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+        Client client = new Client(new WindowsButtonFactory());
+        client.Run();
+
+        client = new Client(new IOSButtonFactory());
+        client.Run();
+
+        client = new Client(new AndroidButtonFactory());
+        client.Run();
+    
